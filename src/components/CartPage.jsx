fbd82d7fca7701b0a11d467ae4c8e841dbb843cf
visalxz69696969 <saovisal12192005@gmail.com>
2025-04-11 16:25:44 +0700

    final working code just lacking data

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -2,14 +2,19 @@ import { useEffect, useState } from "react";
 
 const CartPage = () => {
   const [keyboardDetail, setKeyboardDetail] = useState([]);
-
+  const [showPopup, setShowPopup] = useState(false);
+  const tax = 0.1;
+  const handleCheckout = () => {
+    localStorage.clear();
+    setShowPopup(true);
+  };
   useEffect(() => {
     const cartData = localStorage.getItem("cart");
     if (cartData) {
       setKeyboardDetail(JSON.parse(cartData));
     }
   }, []);
-  const [counter, setCounter] = useState([1,1,1]);
+  const [counter, setCounter] = useState([1, 1, 1]);
   const handleClickPlus = (index) => {
     const temp = [...counter];
     temp[index]++;
@@ -22,21 +27,19 @@ const CartPage = () => {
     temp[index]--;
     setCounter(temp);
   };
-  const calculateTotal=()=>{
-    let total=0;
-    for(let i=0;i<keyboardDetail.length;i++){
-        total+=keyboardDetail[i].price*counter[i];
+  const calculateTotal = () => {
+    let total = 0;
+    for (let i = 0; i < keyboardDetail.length; i++) {
+      total += keyboardDetail[i].price * counter[i];
     }
-    return total
+    return total;
+  };
+  if (!keyboardDetail) {
+    return <p>No data has been passed in</p>;
   }
-  if(!keyboardDetail){
-    return(
-      <p>No data has been passed in</p>
-    )
-  }
-return (
+  return (
     <div
-        style={{
+      style={{
         display: "flex",
         backgroundColor: "black",
         minHeight: "100vh",
@@ -49,7 +52,7 @@ return (
           flexDirection: "column",
           gap: "20px",
           width: "60%",
-          marginTop: "8%",
+          marginTop: "100px",
           alignItems: "center",
         }}
       >
@@ -80,8 +83,8 @@ return (
                   border: "none",
                   justifyContent: "center",
                   borderRadius: "100%",
-                  backgroundColor:"white",
-                  color:"black"
+                  backgroundColor: "white",
+                  color: "black",
                 }}
                 onClick={() => handleClickMinus(i)}
               >
@@ -97,8 +100,8 @@ return (
                   border: "none",
                   justifyContent: "center",
                   borderRadius: "100%",
-                  backgroundColor:"white",
-                  color:"black"
+                  backgroundColor: "white",
+                  color: "black",
                 }}
                 onClick={() => handleClickPlus(i)}
               >
@@ -110,33 +113,109 @@ return (
       </div>
       <div
         style={{
-          width: "40%",
           display: "flex",
-          marginTop: "10%",
-          alignItems: "center",
+          marginTop: "100px",
           flexDirection: "column",
+          marginLeft: "100px",
         }}
       >
-        <h2>Choose Payment Method</h2>
-        <div style={{ display: "flex", gap:"10px" }}>
-          <img
-            src="/public/khqr-5.png"
-            alt=""
-            style={{ width: "100px", objectFit:"contain" }}
-          />
-          <img
-            src="/public/master-card-logo-png_seeklogo-89117.png"
-            alt=""
-            style={{ width: "100px", objectFit:"contain" }}
-          />
-          <img
-            src="/public/visa-logo-png_seeklogo-149684.png"
-            alt=""
-            style={{ width: "100px" , objectFit:"contain"}}
-          />
+        <h1
+          style={{
+            alignSelf: "center",
+            fontSize: "3rem",
+            marginBottom: "50px",
+          }}
+        >
+          Card Info
+        </h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: "10px",
+          }}
+        >
+          <h1>Subtotal:</h1>
+          <h1>{calculateTotal().toFixed(2)}$</h1>
         </div>
-        <h1 style={{marginTop:"100px"}}>{`Total ${calculateTotal().toFixed(2)}$`}</h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: "10px",
+          }}
+        >
+          <h1>Tax:</h1>
+          <h1>{tax * 100}%</h1>
+        </div>
+        <h1>__________________________________________</h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: "10px",
+          }}
+        >
+          <h1 style={{ fontSize: "2rem" }}>Total:</h1>
+          <h1 style={{ fontSize: "2rem" }}>{calculateTotal().toFixed(2)}$</h1>
+        </div>
+        <button
+          style={{
+            backgroundColor: "white",
+            color: "black",
+            borderRadius: "5px",
+            width: "300px",
+            alignSelf: "center",
+            height: "50px",
+            marginTop: "30px",
+            cursor: "pointer",
+          }}
+          onClick={() => handleCheckout()}
+        >
+          Check Out
+        </button>
       </div>
+      {showPopup && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            height: "100vh",
+            width: "100vw",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 999,
+            animation: "fadeIn 0.3s ease",
+          }}
+          onClick={() => setShowPopup(false)}
+        >
+          <img
+            src="/public/images/photo_2025-04-11_02-41-39.jpg"
+            alt="Popup"
+            style={{
+              transform: "scale(0.8)",
+              animation: "popupScale 0.3s ease forwards",
+              height:"500px",
+            }}
+          />
+          <style>
+            {`
+        @keyframes fadeIn {
+          from { opacity: 0; }
+          to { opacity: 1; }
+        }
+
+        @keyframes popupScale {
+          from { transform: scale(0.8); opacity: 0; }
+          to { transform: scale(1); opacity: 1; }
+        }
+      `}
+          </style>
+        </div>
+      )}
     </div>
   );
 };
